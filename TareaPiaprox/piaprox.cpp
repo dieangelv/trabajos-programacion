9a9466f21441ffb342c592e1d283f89912844291
diangelv <diangel@unal.edu.co>
2025-04-12 19:32:41 -0500

    Añado tarea de pi aproximado

---
+++ TareaPiaprox/piaprox.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+double pi_aprox(int n) {
+    double sumatoria = 0.0;
+    for(int k = 0; k <= n; ++k) {
+        double term = (1.0/pow(16,k))*(4/(8*k+1) - 2/(8*k+4) - 1/(8*k+5) - 1/(8*k+6));
+        sumatoria = sumatoria + term;
+    }
+    return sumatoria;
+    
+}
+
+int main() {
+    std::cout.precision(16);
+    std::cout.setf(std::ios::scientific);
+    
+    for(int n = 1; n <= 20; ++n) {
+        double pi_aproximado = pi_aprox(n);
+        double error_relativo = std::abs(1 - pi_aproximado/M_PI);
+        
+        std::cout<< "n =" << n;
+        std::cout<< "pi aprox =" << pi_aproximado;
+        std::cout<< "error relativo" << error_relativo;
+        
+    }
+    return 0;
+}
