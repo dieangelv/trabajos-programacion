4c60101c8bcdc20769246511a9117e112b5a1f88
diangelv <diangelv@unal.edu.co>
2025-04-13 21:21:21 -0500

    Añado código corregido tarea pi aprox

--- TareaPiaprox/piaprox.cpp
+++ TareaPiaprox/piaprox.cpp
@@ -5,7 +5,7 @@
 double pi_aprox(int n) {
     double sumatoria = 0.0;
     for(int k = 0; k <= n; ++k) {
-        double term = (1.0/pow(16,k))*(4/(8*k+1) - 2/(8*k+4) - 1/(8*k+5) - 1/(8*k+6));
+        double term = (1.0/pow(16, k))*(4.0/(8*k+1) - 2.0/(8*k+4) - 1.0/(8*k+5) - 1.0/(8*k+6));
         sumatoria = sumatoria + term;
     }
     return sumatoria;
@@ -20,9 +20,9 @@ int main() {
         double pi_aproximado = pi_aprox(n);
         double error_relativo = std::abs(1 - pi_aproximado/M_PI);
         
-        std::cout<< "n =" << n;
-        std::cout<< "pi aprox =" << pi_aproximado;
-        std::cout<< "error relativo" << error_relativo;
+        std::cout<< "n = " << n << "\n";
+        std::cout<< "pi aprox = " << pi_aproximado << "\n";
+        std::cout<< "error relativo = " << error_relativo << "\n";
         
     }
     return 0;
